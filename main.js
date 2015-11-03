52568adea56c52adfacc97adf50dbf25b29e776b
Neeraj Kohirkar <neerajk1208@gmail.com>
2015-11-02 17:00:39 -0800

    updated class names to make js work properly

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var tasks = document.getElementsByClassName("specificTask");
+var tasks = document.getElementsByClassName("task-content");
 
 var task1 = tasks[0];
 var task2 = tasks[1];
 var task3 = tasks[2];
 
-var activities = document.getElementsByClassName("activity");
+var activities = document.getElementsByClassName("task");
 
 var activity1 = activities[0];
 var activity2 = activities[1];
@@ -26,49 +26,49 @@ console.log(dones)
 
 
 task1.addEventListener("click", function() {
-	activity1.className = "activity edit";
+	activity1.className = "task edit";
 	input1.setSelectionRange(0, input1.value.length)
 });
 
 input1.addEventListener("blur", function() {
-	activity1.className ="activity";
-	task1.className = "specificTask";
+	activity1.className ="task";
+	task1.className = "task-content";
 	task1.innerHTML = input1.value || "1st Priority";
 })
 
 done1.addEventListener("click", function() {
-	task1.className = "specificTask donezo";
+	task1.className = "task-content completed";
 })
 
 
 task2.addEventListener("click", function() {
-	activity2.className = "activity edit";
+	activity2.className = "task edit";
 	input2.setSelectionRange(0, input2.value.length)
 });
 
 input2.addEventListener("blur", function() {
-	activity2.className ="activity";
-	task2.className = "specificTask";
+	activity2.className ="task";
+	task2.className = "task-content";
 	task2.innerHTML = input2.value || "2nd Priority";
 })
 
 done2.addEventListener("click", function() {
-	task2.className = "specificTask donezo";
+	task2.className = "task-content completed";
 })
 
 task3.addEventListener("click", function() {
-	activity3.className = "activity edit";
+	activity3.className = "task edit";
 	input3.setSelectionRange(0, input3.value.length)
 });
 
 input3.addEventListener("blur", function() {
-	activity3.className ="activity"
-	task3.className = "specificTask";
+	activity3.className ="task"
+	task3.className = "task-content";
 	task3.innerHTML = input3.value || "3rd Priority";
 })
 
 done3.addEventListener("click", function() {
-	task3.className = "specificTask donezo";
+	task3.className = "task-content completed";
 })
 
 console.log("hello")
